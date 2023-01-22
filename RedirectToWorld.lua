bb5618a8e4929a08bfe9a3ea115f371bf78ee41b
hwsoftworks <123269183+hwsoftworks@users.noreply.github.com>
2023-01-22 23:21:34 +0000

    Add files via upload

---
+++ RedirectToWorld.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 3101667897 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/hwsoftworks/legendsofspeed/main/script.lua", true))()
+elseif game.PlaceId == 3232996272 then
+  notif("Not Yet Supported Outer Space")
+elseif game.PlaceId == 3276265788 then
+  notif("Not Yet Supported Speed Desert")
+else
+  notif("Not Supported Game")
+  notif("Discord link copied")
+  setclipboard("discord.gg/pfe48nR6p4")
+end
